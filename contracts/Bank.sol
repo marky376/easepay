cf6c8d28124eabeaa4f5a2025f3934877540396d
marky376 <mmutuamark@gmail.com>
2024-11-06 15:31:41 +0300

    Constant improvements in my Bank.sol file

---
+++ contracts/Bank.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Bank {
+    mapping(address => uint) public balances;
+
+    function deposit() public payable {
+        balances[msg.sender] += msg.value;
+    }
+
+    function withdraw(uint amount) public {
+        require(balances[msg.sender] >= amount, "Insufficient balance");
+        balances[msg.sender] -= amount;
+        payable(msg.sender).transfer(amount);
+    }
+}
